de279290ecf046d1c34bf3a729aa0cd27c4d43c5
Henrikh Bandrovskyi <henrikh.bandrovskyi@gmail.com>
2017-01-18 14:13:26 +0200

    Server Model fields update
    Fields updated. Server state choices removed from class Server.
    Name spelling updated.

--- moninag/moninag/settings.py
+++ moninag/moninag/settings.py
@@ -15,7 +15,6 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
 
@@ -27,7 +26,6 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-
 # Application definition
 
 INSTALLED_APPS = [
@@ -37,6 +35,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'server'
 ]
 
 MIDDLEWARE = [
@@ -69,17 +68,15 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'moninag.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': '',
+        'NAME': '',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/1.10/ref/settings/#auth-password-validators
@@ -99,7 +96,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/1.10/topics/i18n/
 
@@ -113,13 +109,12 @@ USE_L10N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
 
 try:
-    from local_settings import *
+    from .local_settings import *
 except ImportError:
     pass
--- moninag/server/models.py
+++ moninag/server/models.py
@@ -1,11 +1,23 @@
 from django.db import models
 
+STATUS_CHOICES = (
+    (1, ("NotSelected")),
+    (2, ("Production")),
+    (3, ("Staging")),
+)
+
+
 class Server(models.Model):
-    STATES = list(
-        'production',
-        'staging'
-    )
+    """
+    Server model with fields:
+    id, name, address, status
+    """
     name = models.CharField(max_length=50)
     address = models.CharField(max_length=200)
-    status = models.CharField(choices=STATES, default=STATES.draft)
-    #user = models.ForeignKey(User, unique=True)
+    status = models.CharField(max_length=100, choices=STATUS_CHOICES, default=0)
+
+    def __str__(self):
+        return "ServerId: {}, ServerName: {}, ServerAddress: {}, ServerStatus {}".format(self.id,
+                                                                                         self.name,
+                                                                                         self.address,
+                                                                                         self.status)
