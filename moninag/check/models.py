da633b5b43e0500cb1e14ad825e790a942e208f3
s0812 <nazarkoblay@gmail.com>
2017-01-18 14:05:16 +0200

    formated model

--- moninag/check/models.py
+++ moninag/check/models.py
@@ -4,9 +4,11 @@ from django.db import models
 
 class Check(models.Model):
 
-	check_name = models.CharField(max_length = 20)
-	check_plugin_name = models.CharField(max_length = 20)
-	check_run_freq = models.IntegerField()
+	name = models.CharField(max_length=20)
+	plugin_name = models.CharField(max_length=20)
+	run_freq = models.IntegerField()
 
 	def __str__(self):
-		return "<Check Name: %s, Plugin Name: %s, Run Frequency: %d>".format(self.check_name, self.check_plugin_name, self.check_run_freq)
+		return "Check Name: %s, Plugin Name: %s, Run Frequency: %d".format(self.name,
+                                                    					   self.plugin_name,
+                                                    					   self.run_freq)
