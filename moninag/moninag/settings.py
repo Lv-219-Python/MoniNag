d9e692e6cb25d1ce02db0323f7e270188ea68f4c
Maksym Humetskyi <humetsky@gmail.com>
2017-02-09 02:56:20 +0200

    Create AuthCheckerMiddleware

--- moninag/moninag/settings.py
+++ moninag/moninag/settings.py
@@ -54,6 +54,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'middleware.authchecker.AuthCheckerMiddleware',
 ]
 
 ROOT_URLCONF = 'moninag.urls'
