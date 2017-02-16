73d678ec42bf52413790bcd6665201506b8d9722
KlubukAI <maaraanas@gmail.com>
2017-02-16 18:46:23 +0200

    plugin testing

---
+++ moninag/tests/unittests/nagplugin/test_model.py
@@ -0,0 +1,58 @@
+from django.test import TestCase
+
+from nagplugin.models import NagPlugin
+
+
+class TestPlugin(TestCase):
+    def test_get_by_id(self):
+        """Ensure that get by id method returns specific plugin using id"""
+
+        result = NagPlugin.get_by_id(1)
+        expected = NagPlugin.objects.get(id=1)
+
+        self.assertEqual(result, expected)
+
+    def test_get(self):
+        """Ensure that get method returns specific plugin using id"""
+
+        result = NagPlugin.get()
+        expected = NagPlugin.objects.all()[0:20]
+
+        self.assertQuerysetEqual(result, map(repr, expected), ordered=False)
+
+    def test_get_by_id_none(self):
+        """Ensure that get_by_id method returns none if plugin does not exist"""
+
+        result = NagPlugin.get_by_id(66)
+        self.assertEqual(result, None)
+
+    def test_get_by_name(self):
+        """Ensure that get_by_name returns all plugins for specific name"""
+
+        result = NagPlugin.get_by_name("check_dns")
+        expected = NagPlugin.objects.filter(name="check_dns")
+        self.assertQuerysetEqual(result, map(repr, expected))
+
+    def test_to_dict(self):
+        """Ensure that to_dict methods builds a proper dict from plugin"""
+
+        plugin = NagPlugin.objects.get(id=1)
+        result = plugin.to_dict()
+        expected = {
+            'id': 1,
+            'name': "check_dns",
+            'template': "/usr/lib/nagios/plugins/check_dns -H {host}",
+        }
+
+        self.assertDictEqual(result, expected)
+
+    def test___str__(self):
+        """Ensure that __str__ method builds a proper str representation of a plugin"""
+
+        plugin = NagPlugin.objects.get(id=1)
+        result = str(plugin)
+        expected = 'Plugin id: {}, name: {}, template: {}'.format(plugin.id,
+                                                                  plugin.name,
+                                                                  plugin.template)
+
+        self.assertEqual(result, expected)
