a55f73b7fae6191a68f12b486328b7c5eb741721
s0812 <nazarkoblay@gmail.com>
2017-02-18 16:19:49 +0200

    added contacts functionality

---
+++ moninag/static/src/ts/app/contacts/edit.component.ts
@@ -0,0 +1,60 @@
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import { ActivatedRoute, Params, Router } from '@angular/router';
+import { Location } from '@angular/common';
+
+
+import { Contact } from './model';
+import { ContactsService } from './service';
+
+import 'rxjs/add/operator/switchMap';
+
+@Component({
+    selector: 'contacts-edit',
+    template: `
+    <div *ngIf="contact"><div>
+    <h5>First Name:</h5>
+    <input [(ngModel)]="contact.first_name" placeholder="{{contact.first_name}}" />
+    <h5>Second Name:</h5>
+    <input [(ngModel)]="contact.second_name" placeholder="{{contact.second_name}}" />
+    <h5>Email:</h5>
+    <input [(ngModel)]="contact.email" placeholder="{{contact.email}}" />
+    <button (click)="goBack()">Back</button>
+    <button (click)="save()">Save</button>
+    <button (click)="delete()">x</button>
+    `,
+    providers: [
+        ContactsService
+    ],
+})
+
+export class ContactsEditComponent {
+
+    constructor(
+        private contactsService: ContactsService,
+        private route: ActivatedRoute,
+        private router: Router,
+        private location: Location
+    ){}
+
+    contact : Contact[];                                 
+    
+    ngOnInit(){
+        this.route.params
+            .switchMap((params: Params) => this.contactsService.getContact(+params['id']))
+            .subscribe(contact => this.contact = contact['response']);
+    }
+    
+    save(){
+        this.contactsService.putContact(this.contact)
+        .subscribe(() => this.goBack())
+    }
+    delete(){
+        this.contactsService.deleteContact(this.contact['id'])
+        .subscribe(() => this.goBack())
+    } 
+
+    goBack(){
+        this.location.back();
+    }    
+}                                           
