1d68a50e8388cfffe727229d750b1b274f7db77a
Zoriana Zubrytska <Zoriana.28@gmail.com>
2017-02-20 00:31:37 +0200

    Changed according to pep8

--- moninag/tests/unittests/service/test_service_model.py
+++ moninag/tests/unittests/service/test_service_model.py
@@ -1,18 +1,47 @@
 from django.test import TestCase
+
 from service.models import Service
 from server.models import Server
 from registration.models import CustomUser
 
+
 class ServiceTest(TestCase):
     def setUp(self):
-        user=CustomUser.objects.create(id=1, first_name="firstname", second_name="secondname", email="email@gmail.com")
-        user1=CustomUser.objects.create(id=2, first_name="firstname2", second_name="secondname2", email="email2@gmail.com")
-        user2=CustomUser.objects.create(id=3, first_name="firstname3", second_name="secondname3", email="email3@gmail.com")
-        server=Server.objects.create(id =1, name="name", address="address", state="Production", user=user)
-        server2=Server.objects.create(id =2, name="name2", address="address2", state="Production", user=user2)
-        Service.objects.create(id =20, name="service1", status="ok", server=server2)
-        Service.objects.create(id =30, name="service2", status="Fail", server=server2)
+        user = CustomUser.objects.create(id=1,
+                                         first_name="firstname",
+                                         second_name="secondname",
+                                         email="email@gmail.com")
 
+        user1 = CustomUser.objects.create(id=2,
+                                          first_name="firstname2",
+                                          second_name="secondname2",
+                                          email="email2@gmail.com")
+
+        user2 = CustomUser.objects.create(id=3,
+                                          first_name="firstname3",
+                                          second_name="secondname3",
+                                          email="email3@gmail.com")
+
+        server = Server.objects.create(id=1,
+                                       name="name",
+                                       address="address",
+                                       state="Production",
+                                       user=user)
+
+        server2 = Server.objects.create(id=2,
+                                        name="name2",
+                                        address="address2",
+                                        state="Production",
+                                        user=user2)
+
+        Service.objects.create(id=20,
+                               name="service1",
+                               status="ok", server=server2)
+
+        Service.objects.create(id=30,
+                               name="service2",
+                               status="Fail",
+                               server=server2)
 
     def test_service_update(self):
         ser1 = Service.objects.get(name="service1")
@@ -23,11 +52,10 @@ class ServiceTest(TestCase):
         server = Server.objects.get(id=1)
         Service.create("service_new", server, "ok")
         service = Service.objects.get(name="service_new")
-        
-        self.assertEqual(service.name,"service_new")
-        self.assertEqual(service.status,"ok")
-        self.assertEqual(service.server, server)
 
+        self.assertEqual(service.name, "service_new")
+        self.assertEqual(service.status, "ok")
+        self.assertEqual(service.server, server)
 
     def test_service_get_by_id(self):
         actual = Service.objects.get(id=30)
@@ -38,19 +66,19 @@ class ServiceTest(TestCase):
         server = Server.objects.get(id=1)
         actual = Service.objects.filter(server=server)
         expect = Service.get_by_server_id(1)
-        self.assertQuerysetEqual(expect,map(repr,actual), ordered=False)
+        self.assertQuerysetEqual(expect, map(repr, actual), ordered=False)
 
     def test_service_get_by_user_id(self):
         user = CustomUser.objects.get(id=3)
         servers = Server.objects.filter(user=user)
         actual = Service.objects.filter(server__in=servers)
         expect = Service.get_by_user_id(3)
-        self.assertQuerysetEqual(expect, map(repr,actual), ordered=False)
+        self.assertQuerysetEqual(expect, map(repr, actual), ordered=False)
 
     def test_service_get_by_statuses(self):
         actual = Service.objects.filter(status__in=["ok"])
         expect = Service.get_by_statuses(["ok"])
-        self.assertQuerysetEqual(expect, map(repr,actual), ordered=False)
+        self.assertQuerysetEqual(expect, map(repr, actual), ordered=False)
 
     def test_service_to_dict(self):
         service = Service.objects.get(id=30)
@@ -61,7 +89,7 @@ class ServiceTest(TestCase):
             'server_id': 2
         }
         expect = service.to_dict()
-        self.assertEqual(expect, actual) 
+        self.assertEqual(expect, actual)
 
     def test_service__str__(self):
         service = Service.objects.get(id=20)
--- moninag/tests/unittests/service/test_service_view.py
+++ moninag/tests/unittests/service/test_service_view.py
@@ -1,12 +1,13 @@
-from django.test import TestCase
-from django.test import Client
-from django.core.urlresolvers import reverse
-from django.contrib.auth import authenticate
 import json
+
+from django.contrib.auth import authenticate
+from django.core.urlresolvers import reverse
+from django.test import Client, TestCase
 
 from registration.models import CustomUser
 from server.models import Server
 from service.models import Service
+
 
 def to_dict(service):
 
@@ -80,7 +81,6 @@ class TestServiceView(TestCase):
 
         self.client = Client()
         self.client.login(username='email@gmail.com', password='qwerty')
-    
 
     def test_get_services(self):
 
@@ -88,13 +88,15 @@ class TestServiceView(TestCase):
         actual_response = self.client.get(url)
 
         servers = Server.objects.filter(user=1)
-        services= Service.objects.filter(server__in=servers)
+        services = Service.objects.filter(server__in=servers)
 
         expected_response = {}
-        expected_response['response'] = [to_dict(service) for service in services]
+        expected_response['response'] = [to_dict(service)
+                                         for service in services]
 
         self.assertEqual(actual_response.status_code, 200)
-        self.assertJSONEqual(actual_response.content.decode('utf-8'), expected_response)
+        self.assertJSONEqual(actual_response.content.decode('utf-8'),
+                             expected_response)
 
     def test_get_service(self):
 
@@ -106,7 +108,8 @@ class TestServiceView(TestCase):
         expected_response['response'] = to_dict(service)
 
         self.assertEqual(actual_response.status_code, 200)
-        self.assertJSONEqual(actual_response.content.decode('utf-8'), expected_response)
+        self.assertJSONEqual(actual_response.content.decode('utf-8'),
+                             expected_response)
 
     def test_get_not_existing_service(self):
 
@@ -114,7 +117,6 @@ class TestServiceView(TestCase):
         actual_response = self.client.get(url)
 
         self.assertEqual(actual_response.status_code, 404)
-
 
     def test_get_not_user_service(self):
 
@@ -130,7 +132,8 @@ class TestServiceView(TestCase):
                            'status': 'ok',
                            'server_id': 10})
 
-        actual_response = self.client.post(url, data=data, content_type='application/json')
+        actual_response = self.client.post(url, data=data,
+                                           content_type='application/json')
         service = Service.objects.get(name='Service')
 
         self.assertEqual(actual_response.status_code, 201)
@@ -143,7 +146,8 @@ class TestServiceView(TestCase):
         url = reverse('service')
         data = json.dumps({'name': 'Service'})
 
-        actual_response = self.client.post(url, data=data, content_type='application/json')
+        actual_response = self.client.post(url, data=data,
+                                           content_type='application/json')
 
         self.assertEqual(actual_response.status_code, 400)
 
@@ -154,7 +158,8 @@ class TestServiceView(TestCase):
                            'status': 'ok',
                            'server_id': 30})
 
-        actual_response = self.client.post(url, data=data, content_type='application/json')
+        actual_response = self.client.post(url, data=data,
+                                           content_type='application/json')
 
         self.assertEqual(actual_response.status_code, 404)
 
@@ -165,7 +170,8 @@ class TestServiceView(TestCase):
                            'status': 'ok',
                            'server_id': 20})
 
-        actual_response = self.client.post(url, data=data, content_type='application/json')
+        actual_response = self.client.post(url, data=data,
+                                           content_type='application/json')
 
         self.assertEqual(actual_response.status_code, 403)
 
@@ -175,7 +181,8 @@ class TestServiceView(TestCase):
         data = json.dumps({'name': 'Service101',
                            'status': 'fail'})
 
-        actual_response = self.client.put(url, data=data, content_type='application/json')
+        actual_response = self.client.put(url, data=data,
+                                          content_type='application/json')
         service = Service.objects.get(id=11)
 
         self.assertEqual(actual_response.status_code, 200)
@@ -187,9 +194,10 @@ class TestServiceView(TestCase):
         url = reverse('service', args=[11])
         data = json.dumps({'name': 'Service101',
                            'status': '15',
-                           'user_id':18 })
+                           'user_id': 18})
 
-        actual_response = self.client.put(url, data=data, content_type='application/json')
+        actual_response = self.client.put(url, data=data,
+                                          content_type='application/json')
 
         self.assertEqual(actual_response.status_code, 400)
 
@@ -199,16 +207,19 @@ class TestServiceView(TestCase):
         data = json.dumps({'name': 'Service101',
                            'status': 'ok'})
 
-        actual_response = self.client.put(url, data=data, content_type='application/json')
+        actual_response = self.client.put(url, data=data,
+                                          content_type='application/json')
 
         self.assertEqual(actual_response.status_code, 404)
+
     def test_put_not_user_service(self):
 
         url = reverse('service', args=[33])
         data = json.dumps({'name': 'Service101',
                            'status': 'ok'})
 
-        actual_response = self.client.put(url, data=data, content_type='application/json')
+        actual_response = self.client.put(url, data=data,
+                                          content_type='application/json')
 
         self.assertEqual(actual_response.status_code, 403)
 
@@ -221,7 +232,7 @@ class TestServiceView(TestCase):
         self.assertEqual(len(Service.objects.all()), 2)
 
     def test_delete_not_user_service(self):
-        
+
         url = reverse('service', args=[33])
         actual_response = self.client.delete(url)
 
@@ -233,6 +244,3 @@ class TestServiceView(TestCase):
         actual_response = self.client.delete(url)
 
         self.assertEqual(actual_response.status_code, 404)
-
-
-    
