5001de460205ecab5aab6f82371a5bbeb0cbc57e
VladLi <edmondantes228@gmail.com>
2017-03-04 15:40:49 +0200

    Updated due to requests.
    pylint ~9.5

--- moninag/tests/unittests/registration/test_view.py
+++ moninag/tests/unittests/registration/test_view.py
@@ -6,36 +6,38 @@ from django.test import TestCase, Client, mock
 from registration.models import CustomUser
 
 
-class Fakeuidb64(object):
-    """Mock uidb64 for password reset"""
+def mock_uidb64(self):
+    """ Mock uidb64 for password reset"""
+    # pylint: disable=unused-argument
 
-    def __init__(self, *args, **kwargs):
-        pass
-
-    def uidb64(self):
-        return '15'
+    return '1000'
 
 
 class FakeToken(object):
-    """Mock token for password reset"""
+    """ Mock token for password reset"""
+    # pylint: disable=too-few-public-methods
 
     def __init__(self, *args, **kwargs):
         pass
 
     def check_token(self, a):
+        # pylint: disable=unused-argument, invalid-name, no-self-use, missing-docstring
         return True
 
 
-class LoginView(TestCase):
+class LoginViewTest(TestCase):
+    # pylint: disable=missing-docstring
+
     def setUp(self):
-        self.user = CustomUser.objects.create(first_name="FirstName",
-                                              second_name="SecondName",
-                                              email="testmail@test.so",
-                                              avatar="avatartest",
-                                              activation_key="testkey",
+        self.user = CustomUser.objects.create(id=1000,
+                                              first_name='FirstName',
+                                              second_name='SecondName',
+                                              email='testmail@test.so',
+                                              avatar='avatartest',
+                                              activation_key='testkey',
                                               is_active=True)
 
-        self.user.set_password("rootroot")
+        self.user.set_password('rootroot')
         self.user.save()
         self.client = Client()
 
@@ -52,13 +54,13 @@ class LoginView(TestCase):
     def test_activate(self):
         """Ensure that activate() method works properly"""
 
-        user = CustomUser.objects.get(email="testmail@test.so")
+        user = CustomUser.objects.get(email='testmail@test.so')
         user.is_active = False
         user.save()
 
         url = reverse('activate', args=['testkey'])
         response = self.client.get(url)
-        user = CustomUser.objects.get(email="testmail@test.so")
+        user = CustomUser.objects.get(email='testmail@test.so')
         expected = 'registration/activate.html'
 
         self.assertEqual(response.status_code, 200)
@@ -66,9 +68,10 @@ class LoginView(TestCase):
         self.assertEqual(user.is_active, True)
 
     def test_already_activated(self):
-        """Ensure that activate() method works properly, if user is already activated"""
+        """Ensure that activate() method works properly,
+           if user is already activated"""
 
-        user = CustomUser.objects.get(email="testmail@test.so")
+        user = CustomUser.objects.get(email='testmail@test.so')
         url = reverse('activate', args=['testkey'])
         response = self.client.get(url)
         expected = 'registration/already_activated.html'
@@ -80,9 +83,9 @@ class LoginView(TestCase):
     def test_login(self):
         """Ensure that login() method works properly"""
 
-        user_data = json.dumps({'email': "testmail@test.so",
-                                'password': "rootroot",
-                                })
+        user_data = json.dumps({'email': 'testmail@test.so',
+                                'password': 'rootroot',
+                               })
         url = reverse('login')
         response = self.client.post(url, data=user_data, content_type='application/json')
 
@@ -93,9 +96,8 @@ class LoginView(TestCase):
 
         self.user.is_active = False
         self.user.save()
-        user_data = json.dumps({'email': "testmail@test.so",
-                                'password': "rootroot",
-                                })
+        user_data = json.dumps({'email': 'testmail@test.so',
+                                'password': 'rootroot'})
 
         url = reverse('login')
         response = self.client.post(url, data=user_data, content_type='application/json')
@@ -105,9 +107,8 @@ class LoginView(TestCase):
     def test_login_invalid_email_password(self):
         """Ensure we can't log in with invalid email/password"""
 
-        user_data = json.dumps({'email': "abyrvalg",
-                                'password': "glavryba",
-                                })
+        user_data = json.dumps({'email': 'abyrvalg',
+                                'password': 'glavryba'})
 
         url = reverse('login')
         response = self.client.post(url, data=user_data, content_type='application/json')
@@ -125,7 +126,7 @@ class LoginView(TestCase):
     def test_logout(self):
         """Ensure we can successfully log out."""
 
-        self.client.login(username="testmail@test.so", password="rootroot")
+        self.client.login(username='testmail@test.so', password='rootroot')
         urllogout = reverse('logout')
         response_out = self.client.get(urllogout)
 
@@ -142,14 +143,15 @@ class LoginView(TestCase):
 
         url = reverse('register_user')
         response = self.client.post(url, data=reg_data, content_type='application/json')
-        expected_json = json.dumps({'error': {},
-                                    'message': 'Thank you for your time. The confirmation code has been sent to '
-                                    'your email. In order to confirm the registration, simply click on '
-                                    'the link given in it.',
-                                    'success': True})
+        expected = json.dumps({'error': {},
+                               'message': 'Thank you for your time. '
+                                          'The confirmation code has been sent to your email. '
+                                          'In order to confirm the registration, simply click on '
+                                          'the link given in it.',
+                               'success': True})
 
         self.assertEqual(response.status_code, 200)
-        self.assertJSONEqual(str(response.content, encoding='utf8'), expected_json)
+        self.assertJSONEqual(str(response.content, encoding='utf8'), expected)
 
     def test_register_user_get(self):
         """Test if we get right render with GET method"""
@@ -160,24 +162,15 @@ class LoginView(TestCase):
         self.assertTemplateUsed(response_render, 'registration/register.html')
 
     def test_already_registered_email(self):
-        """Ensure that we get an error when we try to register with an already registered email"""
-        """First, we register a user"""
-
-        reg_data = json.dumps({'email': 'test@test.test',
-                               'firstName': 'TestName',
-                               'lastName': 'LastName',
-                               'password': 'isecretlyloveunittests'})
-
-        url = reverse('register_user')
-        response = self.client.post(url, data=reg_data, content_type='application/json')
-        self.assertEqual(response.status_code, 200)
-        """Now we try to register that email once more"""
-        new_reg_data = json.dumps({'email': 'test@test.test',
+        """Ensure that we get an error when we try
+           to register with an already registered email"""
+        new_reg_data = json.dumps({'email': 'testmail@test.so',
                                    'firstName': 'Iam',
                                    'lastName': 'Inlove',
                                    'password': 'withunittests'})
 
-        expected_json = json.dumps({'error': 'This email is already registered. Please try another',
+        expected_json = json.dumps({'error': 'This email is already registered. '
+                                             'Please try another',
                                     'message': {},
                                     'success': False})
 
@@ -195,7 +188,8 @@ class LoginView(TestCase):
                                'lastName': 'LastName',
                                'password': 'isecretlyloveunittests'})
 
-        expected_json = json.dumps({'error': "The email address you've entered has not a valid format",
+        expected_json = json.dumps({'error': "The email address you've entered "
+                                             "has not a valid format",
                                     'message': {},
                                     'success': False})
 
@@ -209,21 +203,23 @@ class LoginView(TestCase):
         """Ensure that user can reset his password"""
 
         reg_data = json.dumps({'email': 'testmail@test.so'})
-        expected_json = json.dumps({'error': {},
-                                    'message': 'An email has been sent to testmail@test.so. Please check its inbox to continue reseting password.',
-                                    'success': True})
+        expected = json.dumps({'error': {},
+                               'message': 'An email has been sent to testmail@test.so. '
+                                          'Please check its inbox to continue reseting password.',
+                               'success': True})
 
         url = reverse('reset_password')
         response = self.client.post(url, data=reg_data, content_type='application/json')
 
         self.assertEqual(response.status_code, 200)
-        self.assertJSONEqual(str(response.content, encoding='utf8'), expected_json)
+        self.assertJSONEqual(str(response.content, encoding='utf8'), expected)
 
     def test_request_password_reset_nonexisting_user(self):
-        """Ensure we can't send an email if it is not connected to some user"""
+        """Ensure we can't send an email if it is
+           not connected to some user"""
 
         reg_data = json.dumps({'email': 'nonexistant@test.so'})
-        expected_json = json.dumps({'error': "No user is associated with this email address",
+        expected_json = json.dumps({'error': 'No user is associated with this email address',
                                     'message': {},
                                     'success': False})
 
@@ -246,57 +242,41 @@ class LoginView(TestCase):
         """Ensure that user can't reset password with invalid email"""
 
         reg_data = json.dumps({'email': 'invalid'})
-        expected_json = json.dumps({'error': "The email address you've entered has not a valid format",
-                                    'message': {},
-                                    'success': False})
+        expected = json.dumps({'error': "The email address you've entered has not a valid format",
+                               'message': {},
+                               'success': False})
 
         url = reverse('reset_password')
         response = self.client.post(url, data=reg_data, content_type='application/json')
 
         self.assertEqual(response.status_code, 200)
-        self.assertJSONEqual(str(response.content, encoding='utf8'), expected_json)
+        self.assertJSONEqual(str(response.content, encoding='utf8'), expected)
 
-    @mock.patch('registration.views.urlsafe_base64_decode', Fakeuidb64.uidb64)
+    @mock.patch('registration.views.urlsafe_base64_decode', mock_uidb64)
     @mock.patch('registration.views.default_token_generator', FakeToken)
     def test_confirm_password_reset(self):
         """Ensure we can confirm the password reset"""
 
-        user = CustomUser.objects.create(first_name="TestN",
-                                         second_name="SecondName",
-                                         email="testmail2@test.so",
-                                         avatar="avatartest",
-                                         activation_key="testkey",
-                                         is_active=True)
-
-        user.set_password("testpass")
-        user.save()
-        reg_data = json.dumps({'uidb64': '15',
-                               'token': '15',
+        reg_data = json.dumps({'uidb64': '1000',
+                               'token': 'mocktoken',
                                'password': 'testpass'})
 
-        url = reverse('confirm_password_reset', args=['15', '15'])
+        url = reverse('confirm_password_reset', args=['1000', 'mocktoken'])
         response = self.client.post(url, data=reg_data, content_type='application/json')
         expected = ({'error': {},
-                     'message': "Password has been reset. In 5 seconds you will be redirected to the login page.",
+                     'message': 'Password has been reset. '
+                                'In 5 seconds you will be redirected to the login page.',
                      'success': True})
 
         self.assertEqual(response.status_code, 200)
         self.assertJSONEqual(str(response.content, encoding='utf8'), expected)
 
     def test_confirm_password_reset_link_unavailable(self):
-        """Ensure we get an error when trying to confirm the reset with unworking link"""
+        """Ensure we get an error when trying to confirm
+           the reset with unworking link"""
 
-        user = CustomUser.objects.create(first_name="TestN",
-                                         second_name="SecondName",
-                                         email="testmail2@test.so",
-                                         avatar="avatartest",
-                                         activation_key="testkey",
-                                         is_active=True)
-
-        user.set_password("testpass")
-        user.save()
-        reg_data = json.dumps({'uidb64': '15',
-                               'token': '15',
+        reg_data = json.dumps({'uidb64': '1000',
+                               'token': 'mocktoken',
                                'password': 'testpass'})
 
         url = reverse('confirm_password_reset', args=['wrong', 'wrong'])
