d0ec43eda6082cdefe8e7efb98ee7e40a34848d2
Orest Chuguievets <o.chuguivets@gmail.com>
2017-03-26 12:08:53 +0300

    Fixed comments

--- moninag/static/src/ts/app/servers/service.ts
+++ moninag/static/src/ts/app/servers/service.ts
@@ -30,13 +30,13 @@ export class ServersService {
     putServer(server: any): Observable<Server[]> {
         let headers = new Headers({ 'Content-Type': 'application/json' });
         let options = new RequestOptions({ headers: headers });
-        let edited_server = JSON.stringify({
+        let updatedServer = JSON.stringify({
             name: server.name,
             address: server.address,
             state: server.state
         });
 
-        return this.http.put(`${this.serversUrl}${server['id']}/`, edited_server, options)
+        return this.http.put(`${this.serversUrl}${server['id']}/`, updatedServer, options)
             .map((response: any) => {
 
                 if (response.status === 200) {
@@ -72,20 +72,20 @@ export class ServersService {
     deactivate(server: Server): Observable<Server[]> {
         let headers = new Headers({ 'Content-Type': 'application/json' });
         let options = new RequestOptions({ headers: headers });
-        let edited_server = {
+        let updatedServer = {
             state: false
         }
-        return this.http.put(`${this.serversUrl}/${server['id']}/`, JSON.stringify(edited_server), options)
+        return this.http.put(`${this.serversUrl}/${server['id']}/`, JSON.stringify(updatedServer), options)
             .catch((error: any) => Observable.throw(error.json().error || 'Server error'));
     }
 
     activate(server: Server): Observable<Server[]> {
         let headers = new Headers({ 'Content-Type': 'application/json' });
         let options = new RequestOptions({ headers: headers });
-        let edited_server = {
+        let updatedServer = {
             state: true
         }
-        return this.http.put(`${this.serversUrl}/${server['id']}/`, JSON.stringify(edited_server), options)
+        return this.http.put(`${this.serversUrl}/${server['id']}/`, JSON.stringify(updatedServer), options)
             .catch((error: any) => Observable.throw(error.json().error || 'Server error'));
     }
 
--- moninag/static/src/ts/app/services/services.service.ts
+++ moninag/static/src/ts/app/services/services.service.ts
@@ -40,31 +40,31 @@ export class ServicesService {
     update(service: Service): Observable<Service[]> {
         let headers = new Headers({ 'Content-Type': 'application/json' });
         let options = new RequestOptions({ headers: headers });
-        let updated_service = {
+        let updatedService = {
             name: service.name
         }
 
-        return this.http.put(`${this.servicesUrl}/${service['id']}/`, JSON.stringify(updated_service), options)
+        return this.http.put(`${this.servicesUrl}/${service['id']}/`, JSON.stringify(updatedService), options)
             .catch((error: any) => Observable.throw(error.json().error || 'Server error'));
     }
 
     deactivate(service: Service): Observable<Service[]> {
         let headers = new Headers({ 'Content-Type': 'application/json' });
         let options = new RequestOptions({ headers: headers });
-        let updated_service = {
+        let updatedService = {
             state: false
         }
-        return this.http.put(`${this.servicesUrl}/${service['id']}/`, JSON.stringify(updated_service), options)
+        return this.http.put(`${this.servicesUrl}/${service['id']}/`, JSON.stringify(updatedService), options)
             .catch((error: any) => Observable.throw(error.json().error || 'Server error'));
     }
 
     activate(service: Service): Observable<Service[]> {
         let headers = new Headers({ 'Content-Type': 'application/json' });
         let options = new RequestOptions({ headers: headers });
-        let updated_service = {
+        let updatedService = {
             state: true
         }
-        return this.http.put(`${this.servicesUrl}/${service['id']}/`, JSON.stringify(updated_service), options)
+        return this.http.put(`${this.servicesUrl}/${service['id']}/`, JSON.stringify(updatedService), options)
             .catch((error: any) => Observable.throw(error.json().error || 'Server error'));
     }
     remove(id: number): Observable<Service[]> {
@@ -76,12 +76,12 @@ export class ServicesService {
         const url = `${this.servicesUrl}/`;
         let headers = new Headers({ 'Content-Type': 'application/json' });
         let options = new RequestOptions({ headers: headers });
-        let new_service = {
+        let newService = {
             name: service.name,
             status: service.status,
             server_id: service.server_id,
         }
-        return this.http.post(url, new_service, options)
+        return this.http.post(url, newService, options)
             .catch((error: any) => Observable.throw(error.json().error || 'Server error'));
     }
 
